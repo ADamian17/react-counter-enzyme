d23e25e7f108d672dd4ab19ffade5f7cf4d0fa6f
ADamian17 <adonisdesuvikdo@gmail.com>
2022-02-02 21:56:57 -0800

    before ejecting

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,31 @@
-import { render, screen } from '@testing-library/react';
+import Enzyme, { shallow } from 'enzyme';
+import EnzymeAdaptor from '@wojtekmaj/enzyme-adapter-react-17';
+
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+Enzyme.configure({ 
+  adapter: new EnzymeAdaptor() 
+});
+
+test('renders without error', () => {
+  const wrapper = shallow(<App/>);
+  const appComponent = wrapper.find(`[data-test="component-app"]`);
+
+  expect(appComponent.length).toBe(1);
+});
+
+test('renders increment btn', () => {
+  
+});
+
+test('renders counter display', () => {
+  
+});
+
+test('counter display starts at 0', () => {
+  
+});
+
+test('clicking btn increments counter display', () => {
+  
 });
